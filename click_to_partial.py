699a2a6982d6cd698e89cec2a3644d4c44606add
Filipe Pina <filipe.c.pina@gmail.com>
2016-12-15 03:42:42 +0000

    added support for full path

--- click_to_partial.py
+++ click_to_partial.py
@@ -5,33 +5,82 @@ import os
 
 class ClickToPartial(sublime_plugin.TextCommand):
     def run(self, edit, event):
-      vector = (event["x"], event["y"])
-      point = self.view.window_to_text(vector)
-      region = self.view.word(point)
-      word = self.view.substr(region)
-
       if (len(sublime.active_window().folders()) == 0):
         return
 
-      base_folder = sublime.active_window().folders()[0]
+      vector = (event["x"], event["y"])
+      point = self.view.window_to_text(vector)
+      region = self.view.word(point)
+      line_range = self.view.line(region)
+      line_content = self.view.substr(line_range).strip()
 
-      # Find the first file that matches path on file folder
-      for root, dirs, files in os.walk(sublime.active_window().extract_variables()["file_path"]):
-        for file in files:
-          if file.startswith("_" + word):
-             file_name = os.path.join(root, file)
-             sublime.active_window().open_file(file_name)
-             return
+      matched = self.get_string(line_content)
+      if matched:
+        partial_path = matched.group(1)
+        print(partial_path)
+        file_name = partial_path
+        pattern = re.compile(".+\/(.+)$")
+        file_name_tmp = pattern.match(partial_path)
+        if file_name_tmp:
+          file_name = file_name_tmp.group(1)
+      else:
+        return
 
-      # Find the first file that matches path on entire project folder
-      for root, dirs, files in os.walk(base_folder):
-        for file in files:
-          if file.startswith("_" + word):
-            file_name = os.path.join(root, file)
-            sublime.active_window().open_file(file_name)
+
+      # Tries to open absolute path
+      pattern = re.compile("(.+)\/.+$")
+      dir_path = pattern.match(partial_path)
+      if dir_path:
+        dir_path = dir_path.group(1)
+
+        dir_path = self.remove_slash(dir_path)
+        file_name = self.remove_slash(file_name)
+
+        pattern = re.compile("(^.*app/views)")
+        proj_root = pattern.match(self.view.file_name())
+        if proj_root:
+          proj_root = proj_root.group(1)
+
+          walk_path = os.path.join(proj_root, dir_path)
+          if self.look_for_file(file_name, walk_path):
             return
 
+
+      # Find the first file that matches path on file folder
+      walk_path = sublime.active_window().extract_variables()["file_path"]
+      if self.look_for_file(file_name, walk_path):
+        return
+
+
+      # Last chance: find the first file that matches path on entire project folder
+      base_folder = sublime.active_window().folders()[0]
+      self.look_for_file(file_name, base_folder)
       return
+
+    def get_string(self, line_content):
+      pattern = re.compile("^.*render.*?['\"](.*?)['\"].*$")
+      matched = pattern.match(line_content)
+      if matched:
+        return matched
+      return False
+
+    def remove_slash(self, str):
+      if str.startswith("/"):
+        return str[1:]
+      else:
+        return str
+      return False
+
+    def look_for_file(self, file_name, walk_path):
+      walk = os.walk(walk_path)
+      for root, dirs, files in walk:
+        for file in files:
+          if file.startswith("_" + file_name):
+             file_name = os.path.join(root, file)
+             sublime.active_window().open_file(file_name)
+             return True
+      return False
+
       
     def want_event(self):
       return True
